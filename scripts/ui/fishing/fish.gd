978f6a23f93262556df8b256369a7fc307d5e236
YannoGH <yan.fun@outlook.de>
2026-03-25 21:00:45 +0100

    minor additions for fishing

---
+++ scripts/ui/fishing/fish.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+@export var target: Node
+
+var vectorToTarget: Vector2
+@export var speed: float
---
+++ scripts/ui/fishing/target.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var keepInArea: Polygon2D
+
+func newPos():
+	pass
+
+func _ready() -> void:
+	pass
